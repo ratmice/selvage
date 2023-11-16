8e3ad96579199972837c6a94050ee310dc4e6acd
matt rice <ratmice@gmail.com>
2023-11-16 10:29:48 -0800

    Remove unused serde_json error

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,6 @@ pub use svg::Selvage;
 mod shape_transform;
 pub use shape_transform::ShapeTransform;
 
-#[derive(thiserror::Error, Debug)]
-enum Error {
-    #[error("Failed in serde_json")]
-    SerdeJson(#[from] serde_json::Error),
-}
-
 #[cfg(test)]
 mod tests {
     //use super::*;
